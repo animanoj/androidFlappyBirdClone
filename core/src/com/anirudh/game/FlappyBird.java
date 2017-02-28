9050a2e87dffbfb8d5b2b2ac11570aae92b3639c
animanoj <amanoj2@illinois.edu>
2017-02-28 16:40:18 -0600

    added gravity to bird

--- core/src/com/anirudh/game/FlappyBird.java
+++ core/src/com/anirudh/game/FlappyBird.java
@@ -2,58 +2,82 @@ package com.anirudh.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+
 class FlappyBird extends ApplicationAdapter {
-	private SpriteBatch batch;
-	private Texture background;
+    private SpriteBatch batch;
 
-	private Texture[] birds;
-	private Animation<TextureRegion> birdAnimation;
-	private float stateTime;
+    private Texture background;
+    private Texture[] birds;
 
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		background = new Texture("bg.png");
+    private Animation<TextureRegion> birdAnimation;
+    private float stateTime;
 
-		birds = new Texture[2];
-		birds[0] = new Texture("bird_0.png");
-		birds[1] = new Texture("bird_1.png");
+    private float birdY;
+    private float velocity;
+    private float gravity;
 
-		TextureRegion[] birdFrames = new TextureRegion[2];
-		birdFrames[0] = new TextureRegion(birds[0]);
-		birdFrames[1] = new TextureRegion(birds[1]);
+    private int gameState;
 
-		birdAnimation = new Animation<TextureRegion>(0.08f, birdFrames);
-		stateTime = 0f;
-	}
+    @Override
+    public void create () {
+        batch = new SpriteBatch();
+        background = new Texture("bg.png");
 
-	@Override
-	public void render () {
-		if(Gdx.input.justTouched()) {
-			Gdx.app.log("Touched", "Yes");
-		}
+        birds = new Texture[2];
+        birds[0] = new Texture("bird_0.png");
+        birds[1] = new Texture("bird_1.png");
 
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		stateTime = (stateTime + Gdx.graphics.getDeltaTime()) % 0.2f;
-		TextureRegion currentFrame = birdAnimation.getKeyFrame(stateTime, true);
+        TextureRegion[] birdFrames = new TextureRegion[2];
+        birdFrames[0] = new TextureRegion(birds[0]);
+        birdFrames[1] = new TextureRegion(birds[1]);
 
-		batch.begin();
-		batch.draw(background, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-		batch.draw(currentFrame, (Gdx.graphics.getWidth() / 2) - (currentFrame.getRegionWidth() / 2), (Gdx.graphics.getHeight() / 2) - (currentFrame.getRegionHeight() / 2));
+        birdAnimation = new Animation<TextureRegion>(0.1f, birdFrames);
+        stateTime = 0f;
 
-		batch.end();
-	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		background.dispose();
-		birds[0].dispose();
-		birds[1].dispose();
-	}
+        birdY = (Gdx.graphics.getHeight() / 2) - (birds[0].getHeight() / 2);
+        velocity = 0f;
+        gravity = 2f;
+
+        gameState = 0;
+    }
+
+    @Override
+    public void render () {
+        if(Gdx.input.justTouched()) {
+            gameState = 1;
+        }
+
+        stateTime = (stateTime + Gdx.graphics.getDeltaTime()) % 0.2f;
+        TextureRegion currentFrame = birdAnimation.getKeyFrame(stateTime, true);
+
+        if(gameState != 0) {
+            if(Gdx.input.justTouched()) {
+                velocity = -30;
+            }
+
+            if(birdY > 0 || velocity < 0) {
+                velocity += gravity;
+                birdY -= velocity;
+            }
+        }
+
+        batch.begin();
+
+        batch.draw(background, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        batch.draw(currentFrame, (Gdx.graphics.getWidth() / 2) - (currentFrame.getRegionWidth() / 2), birdY);
+
+        batch.end();
+    }
+
+    @Override
+    public void dispose () {
+        batch.dispose();
+        background.dispose();
+        birds[0].dispose();
+        birds[1].dispose();
+    }
 }
